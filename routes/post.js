f72411010ff9fdefea8d8e035b2ea75be82a3798
Kunal Tankra <kunaltankra2002@gmail.com>
2024-11-16 11:12:28 +0530

    remove access token authentication from getAllPosts

--- routes/post.js
+++ routes/post.js
@@ -5,7 +5,7 @@ import { isAuthenticated } from '../middlewares/authToken.js'
 const router = express.Router()
 
 router.post('/api/v1/auth/post', createPost)
-router.get('/user/api/v1/post', isAuthenticated, getAllPosts)
+router.get('/user/api/v1/post', getAllPosts)
 router.get('/api/v1/auth/post/:post_id', isAuthenticated, getPostDetails)
 router.get('/api/v1/auth/category-post/:category_id', getCategoryPosts)
 
