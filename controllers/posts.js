aba91ffcbcf40a8467b4c7892018abdd3dbbba84
Kunal Tankra <kunaltankra2002@gmail.com>
2024-11-16 21:10:36 +0530

    modified getAllPosts : get bookmark status of posts according to user

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,4 @@
+import { isAuthenticated } from "../middlewares/authToken.js"
 import Bookmarks from "../models/bookmarks.js"
 import Posts from "../models/posts.js"
 import { sendError, sendInternalServerError } from "../utils/error.js"
@@ -33,13 +34,29 @@ export const getAllPosts = async (req, res) => {
         const skip = (page - 1) * limit;
 
         const r1 = Posts.countDocuments().skip(page * limit)
-        const r2 = Posts.find().skip(skip).limit(limit).sort({ createdAt: -1 })
+        const r2 = Posts.find().skip(skip).limit(limit).sort({ createdAt: -1 }).populate('user_id', 'first_name last_name email').populate('category').lean()
 
         const [next, posts] = await Promise.all([r1, r2])
 
+        const data = posts.map(({ user_id, ...post }) => ({ ...post, user: { _id: user_id._id, name: (user_id.first_name && user_id.last_name) ? `${user_id.first_name} ${user_id.last_name}` : user_id.email.split('@')[0] }, isBookmarked: false }))
+
+        // set bookmark status by user
+        const { user_id } = req.body
+        if (user_id) {
+            // get all bookmarks of user
+            const bookmarkedPosts = (await Bookmarks.find({ user_id })).map(item => item.post_id)
+
+            // check and set bookmark status in data 
+            for (const post of data) {
+                if (bookmarkedPosts.toString().includes(post._id.toString())) {
+                    post.isBookmarked = true
+                }
+            }
+        }
+
         res.send({
             success: true,
-            results: posts,
+            data,
             next: next ? true : false
         })
     } catch (error) {
